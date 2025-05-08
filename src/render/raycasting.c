c804390f44c08e8387924febd09d910be65591d2
VotreNom <VotreEmail>
2025-05-08 12:31:33 +0200

    oui

--- src/render/raycasting.c
+++ src/render/raycasting.c
@@ -2,5 +2,14 @@
 
 void	raycasting(t_game *game)
 {
+	int mapX;
+	int mapY;
+	double rayPosX;
+	double rayPosY;
+
+	rayPosX = game->player.x;
+	rayPosY = game->player.y;
+	mapX = (int)game->player.x;
+	mapY = (int)game->player.y;
 	
 }
