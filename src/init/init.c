967d2958c45f8c3c3a9463f3a52126d8e002fcd7
VotreNom <VotreEmail>
2025-05-12 11:58:18 +0200

    salut

--- src/init/init.c
+++ src/init/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nadahman <nadahman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yaoberso <yaoberso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 19:09:24 by nas               #+#    #+#             */
-/*   Updated: 2025/05/12 10:35:50 by nadahman         ###   ########.fr       */
+/*   Updated: 2025/05/12 11:52:32 by yaoberso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ void init_player(t_game *game)
 				if (map[x][y] == 'N')
 				{
 					game->player.dir_x = 0;
-					game->player.dir_y = -1;
+					game->player.dir_y = 0.66;
 					game->player.plane_x = 10;
 					game->player.plane_y = 0;
 				}
@@ -75,7 +75,7 @@ void init_player(t_game *game)
 				{
 					game->player.dir_x = 0;
 					game->player.dir_y = 1;
-					game->player.plane_x = -10;
+					game->player.plane_x = -0.66;
 					game->player.plane_y = 0;
 				}
 				else if (map[x][y] == 'E')
@@ -83,14 +83,14 @@ void init_player(t_game *game)
 					game->player.dir_x = 1;
 					game->player.dir_y = 0;
 					game->player.plane_x = 0;
-					game->player.plane_y = 10;
+					game->player.plane_y = 0.66;
 				}
 				else if (map[x][y] == 'W')
 				{
 					game->player.dir_x = -1;
 					game->player.dir_y = 0;
 					game->player.plane_x = 0;
-					game->player.plane_y = 10;
+					game->player.plane_y = -0.66;
 				}
 				return;
 			}
--- src/render/moove.c
+++ src/render/moove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   moove.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nadahman <nadahman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yaoberso <yaoberso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 10:46:36 by nadahman          #+#    #+#             */
-/*   Updated: 2025/05/12 10:57:40 by nadahman         ###   ########.fr       */
+/*   Updated: 2025/05/12 11:49:57 by yaoberso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 #include "../include/cub3d.h"
 
 
-int keycode(int keycode, t_game *game)
-{
-	if (keycode == KEY_W)
-		moove_up(keycode, game);
-	if (keycode == KEY_A)
-		moove_left(keycode, game);
-	if (keycode == KEY_S)
-		moove_down(keycode, game);
-	if (keycode == KEY_D)
-		moove_right(keycode, game);
-	if (keycode == KEY_ESC)
-		exit_escape(keycode, game);
-}
+// int keycode(int keycode, t_game *game)
+// {
+// 	if (keycode == KEY_W)
+// 		moove_up(keycode, game);
+// 	if (keycode == KEY_A)
+// 		moove_left(keycode, game);
+// 	if (keycode == KEY_S)
+// 		moove_down(keycode, game);
+// 	if (keycode == KEY_D)
+// 		moove_right(keycode, game);
+// 	if (keycode == KEY_ESC)
+// 		exit_escape(keycode, game);
+// }
 
-int moove_up(int keycode, t_game *game)
-{
-	if (keycode == KEY_W)
+// int moove_up(int keycode, t_game *game)
+// {
+// 	if (keycode == KEY_W)
 		
-}
+// }
